2ff97ca1f20ff001caf3a82f7bb7fbc5d5324c59
Ashwanth A R <ashwanth1109@gmail.com>
2019-01-29 22:45:49 +0530

    Overwriting Next's app js

---
+++ pages/_app.js
@@ -0,0 +1,47 @@
+// ------------------------------------------------------------
+// import dependencies
+// ------------------------------------------------------------
+import App, { Container } from "next/app"; // Overwriting default App.js from Next
+import { createStore } from "redux"; // redux method for creating a store of reducers
+import reducers from "../lib/redux/store"; // get all reducers from store
+import { Provider } from "react-redux"; // get Provider component for making store available
+import withRedux from "next-redux-wrapper"; // redux wrapper component needed for redux in Next JS
+
+// ------------------------------------------------------------
+// makeStore function needed for redux wrapper component
+// ------------------------------------------------------------
+const makeStore = initialState => createStore(reducers, initialState);
+
+// ------------------------------------------------------------
+// Custom App component - from Next JS documentation
+// ------------------------------------------------------------
+class MyApp extends App {
+    // ------------------------------------------------------------
+    // get initial props from server side rendering
+    // ------------------------------------------------------------
+    static async getInitialProps({ Component, router, ctx }) {
+        let pageProps = {};
+        if (Component.getInitialProps) {
+            try {
+                pageProps = await Component.getInitialProps(ctx);
+            } catch (err) {
+                console.error(err);
+            }
+        }
+        return { pageProps };
+    }
+
+    render = ({ Component, pageProps, store } = this.props) => (
+        <Container>
+            {/* provider component makes store availabe to app */}
+            <Provider store={store}>
+                {/* pass on props as is */}
+                <Component {...pageProps} />
+            </Provider>
+        </Container>
+    );
+}
+// ------------------------------------------------------------
+// export App with redux wrapper
+// ------------------------------------------------------------
+export default withRedux(makeStore)(MyApp);

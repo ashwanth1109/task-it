873aed3cb399b3f1e1379887c5a4bdab5fd1ced5
Ashwanth A R <ashwanth1109@gmail.com>
2019-01-29 22:02:47 +0530

    Created a common styles file

---
+++ styles/style.js
@@ -0,0 +1,18 @@
+export const common = {
+    fScreen: {
+        width: "100vw",
+        height: "100vh"
+    },
+    row: {
+        display: "flex",
+        flexDirection: "row"
+    },
+    col: {
+        display: "flex",
+        flexDirection: "column"
+    },
+    center: {
+        justifyContent: "center",
+        alignItems: "center"
+    }
+};

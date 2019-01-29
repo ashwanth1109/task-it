8e9bd324f20f9cb3645d0b69e97479ed10a0daa0
Ashwanth A R <ashwanth1109@gmail.com>
2019-01-30 01:04:14 +0530

    Created index page with sign up form and logo

--- pages/index.js
+++ pages/index.js
@@ -7,21 +7,28 @@ import { index as s } from "../styles/component";
 import color from "../styles/color";
 import withRedux from "../lib/redux/withRedux";
 import LabelInput from "../components/LabelInput";
+import Button from "../components/Button";
 // ------------------------------------------------------------
 // index page stateless component
 // ------------------------------------------------------------
 class Index extends React.Component {
+    // component state => highlight
     state = {
         highlight: false
     };
+    // function to highlight logo
     highlightLogo = () => this.setState({ highlight: true });
+    // function to un-highlight logo
     unHighlightLogo = () => this.setState({ highlight: false });
+    // navigate to sign up page
+    goToSignUp = () => console.log(`go to sign up page`);
+    // render function
     render({ highlight } = this.state) {
         return (
             <div style={s.container}>
                 <Logo
-                    background={highlight ? color.orange : color.cream}
-                    check={highlight ? color.cream : color.orange}
+                    background={highlight ? color.orange : color.white}
+                    check={highlight ? color.white : color.orange}
                     animationTiming={1}
                 />
                 <Card style={s.cardContainer}>
@@ -38,6 +45,15 @@ class Index extends React.Component {
                         onFocus={this.highlightLogo}
                         onBlur={this.unHighlightLogo}
                     />
+                    <Button style={s.button}>Sign In</Button>
+                    <div style={s.signUp}>
+                        <div
+                            style={{ cursor: "pointer" }}
+                            onClick={this.goToSignUp}
+                        >
+                            Dont have an account? Create one
+                        </div>
+                    </div>
                 </Card>
             </div>
         );

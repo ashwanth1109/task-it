370922e8a80946279aed144b553edfc04b5c4ad7
Ashwanth A R <ashwanth1109@gmail.com>
2019-01-29 23:49:37 +0530

    Added component styles for card and label input

--- styles/component.js
+++ styles/component.js
@@ -1,7 +1,7 @@
 // ------------------------------------------------------------
 // import commonly used styles
 // ------------------------------------------------------------
-import { common } from "./style";
+import { common, font } from "./style";
 import color from "./color";
 // ------------------------------------------------------------
 // index page
@@ -15,17 +15,39 @@ export const index = {
     },
     cardContainer: {
         margin: "15px 0",
-        padding: "20px"
+        ...font.Lato
     }
 };
 // ------------------------------------------------------------
 // card component
 // ------------------------------------------------------------
 export const card = {
+    backgroundColor: "#fff",
+    border: "1px solid #ddd",
+    boxShadow: "0 1px 3px rgba(50,50,50,0.08)",
+    borderRadius: "4px"
+};
+// ------------------------------------------------------------
+// labelInput component
+// ------------------------------------------------------------
+export const labelInput = {
     container: {
-        backgroundColor: "#fff",
-        border: "1px solid #ddd",
-        boxShadow: "0 1px 3px rgba(50,50,50,0.08)",
-        borderRadius: "4px"
+        ...common.row
+    },
+    label: {
+        width: "60px",
+        height: "60px",
+        ...common.fCenter
+    },
+    icon: {
+        height: "30px"
+    },
+    input: {
+        width: "300px",
+        height: "60px",
+        border: "0px",
+        fontSize: "16px",
+        lineHeight: "15px",
+        color: "#666"
     }
 };

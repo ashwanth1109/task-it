382e09b66653fa277bacf885be9526c77a6d3538
Ashwanth A R <ashwanth1109@gmail.com>
2019-01-29 22:46:03 +0530

    Overwriting Next's document

---
+++ pages/_document.js
@@ -0,0 +1,44 @@
+// ------------------------------------------------------------
+// import dependencies
+// ------------------------------------------------------------
+import Document, { Head, Main, NextScript } from "next/document"; // Import default Document from Next JS
+// ------------------------------------------------------------
+// Customizing the default Document
+// ------------------------------------------------------------
+class MyDoc extends Document {
+    render() {
+        return (
+            <html lang="en">
+                <Head>
+                    <meta charSet="utf-8" />
+                    <meta
+                        name="viewport"
+                        content="width=device-width, initial-scale=1, shrink-to-fit=no"
+                    />
+                    <link
+                        href="https://fonts.googleapis.com/css?family=Lato:300,400"
+                        rel="stylesheet"
+                    />
+                    {/* global styles go here - also hover, keyframe animations etc. */}
+                    <style>{`
+                        * {
+                            margin: 0;
+                            padding: 0;
+                        }
+                        
+                    `}</style>
+                </Head>
+                {/* Add body styles here */}
+                <body>
+                    {/* from next js documentation */}
+                    <Main />
+                    <NextScript />
+                </body>
+            </html>
+        );
+    }
+}
+// ------------------------------------------------------------
+// export customized document
+// ------------------------------------------------------------
+export default MyDoc;

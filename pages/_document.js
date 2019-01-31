034669806f729c0f7cc53030ab3f4e2c701bf6bf
Ashwanth A R <ashwanth1109@gmail.com>
2019-01-31 07:31:13 +0530

    Expanded btn class for 2 different styles

--- pages/_document.js
+++ pages/_document.js
@@ -2,6 +2,7 @@
 // import dependencies
 // ------------------------------------------------------------
 import Document, { Head, Main, NextScript } from "next/document"; // Import default Document from Next JS
+import color from "../styles/color";
 // ------------------------------------------------------------
 // Customizing the default Document
 // ------------------------------------------------------------
@@ -24,20 +25,27 @@ class MyDoc extends Document {
                         * {
                             margin: 0;
                             padding: 0;
+                            box-sizing: border-box;
                         }
                         *:focus {
                             outline: none;
                         }
-                        .btn {
-                            background-color: #2b88d9;
+                        .btn1 {
+                            background-color: ${color.royalBlue};
                         }
-                        .btn:hover {
-                            background-color: #4094dd;
+                        .btn1:hover {
+                            background-color: ${color.lightRoyal};
+                        }
+                        .btn2 {
+                            background-color: ${color.salmon}
+                        }
+                        .btn2:hover {
+                            background-color: ${color.coral}
                         }
                     `}</style>
                 </Head>
                 {/* Add body styles here */}
-                <body>
+                <body style={{ backgroundColor: color.royalBlue }}>
                     {/* from next js documentation */}
                     <Main />
                     <NextScript />

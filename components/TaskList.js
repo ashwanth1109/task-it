d28083bf3b9521396c1f798b7312c36b4db5fd22
Ashwanth A R <ashwanth1109@gmail.com>
2019-01-31 23:44:43 +0530

    Created TaskList component to list all tasks in the array

---
+++ components/TaskList.js
@@ -0,0 +1,28 @@
+// ------------------------------------------------------------
+// import dependencies
+// ------------------------------------------------------------
+import { taskList as s } from "../styles/component";
+import withRedux from "../lib/redux/withRedux";
+import Task from "./Task";
+// ------------------------------------------------------------
+// TaskList stateful component
+// ------------------------------------------------------------
+const TaskList = ({ user }) => {
+    const tasks = user.tasks || [];
+    return (
+        <div style={s.container}>
+            {tasks.map((task, id) => (
+                <Task
+                    key={id}
+                    checked={task.checked}
+                    description={task.description}
+                    date={task.date}
+                />
+            ))}
+        </div>
+    );
+};
+// ------------------------------------------------------------
+// export TaskList
+// ------------------------------------------------------------
+export default withRedux(TaskList, true, false);

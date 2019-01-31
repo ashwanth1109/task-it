a4d705f34ed3a9eb94c41baadeca8601576c4b99
Ashwanth A R <ashwanth1109@gmail.com>
2019-01-31 23:44:24 +0530

    Added card that displays task list with mock data

--- pages/dashboard.js
+++ pages/dashboard.js
@@ -7,6 +7,7 @@ import { dashboard as s } from "../styles/component";
 import Card from "../components/Card";
 import * as moment from "moment";
 import AddTask from "../components/AddTask";
+import TaskList from "../components/TaskList";
 // ------------------------------------------------------------
 // dashboard page stateless component
 // ------------------------------------------------------------
@@ -16,7 +17,29 @@ class Dashboard extends React.Component {
             // Router.push("/");
             updateState("USER", {
                 name: "Test User",
-                tasks: [],
+                tasks: [
+                    {
+                        description: "Complete wireframes for Task It",
+                        date: "January 31, 2019",
+                        checked: true
+                    },
+                    {
+                        description: "Complete designs for Task It",
+                        date: "January 31, 2019",
+                        checked: true
+                    },
+                    {
+                        description: "Complete frontend for Task It",
+                        date: "January 31, 2019",
+                        checked: false
+                    },
+                    {
+                        description:
+                            "This is a super long task and it should get cut off by the overflow property. This is a super long task and it should get cut off by the overflow property.",
+                        date: "January 31, 2019",
+                        checked: false
+                    }
+                ],
                 username: "test"
             });
         }
@@ -33,6 +56,9 @@ class Dashboard extends React.Component {
                             <div style={s.date}>{moment().format("LL")}</div>
                         </div>
                         <AddTask />
+                    </Card>
+                    <Card style={{ ...s.cardContainer, marginTop: "50px" }}>
+                        <TaskList />
                     </Card>
                 </div>
             );

70e03881045a5bee8213fca316074b8949210b1d
Ashwanth A R <ashwanth1109@gmail.com>
2019-01-31 23:43:22 +0530

    Added lighGray, coral, gainsboro and grey colors

--- styles/color.js
+++ styles/color.js
@@ -4,14 +4,16 @@
 const color = {
     white: "#fff",
     cream: "#fafafa",
-    lightGray: "#eee",
+    lightGray: "#BDBDBD",
     orange: "#F66031",
     blue: "#2b88d9",
     lightBlue: "#4094dd",
     royalBlue: "#595ce2",
     lightRoyal: "#686AE4",
     salmon: "#f27475",
-    coral: "#F38081"
+    coral: "#F38081",
+    gainsboro: "#d6d6e4",
+    grey: "#9E9E9E"
 };
 // ------------------------------------------------------------
 // export color

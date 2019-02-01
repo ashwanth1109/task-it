602857a1f4857ed6ddc1638645aeee57efb3db62
Ashwanth A R <ashwanth1109@gmail.com>
2019-02-01 10:13:15 +0530

    Task component toggles state between input and field

--- components/Task.js
+++ components/Task.js
@@ -2,59 +2,81 @@
 // import dependencies
 // ------------------------------------------------------------
 import PropTypes from "prop-types";
+import moment from "moment";
 import Logo from "./Logo";
 import color from "../styles/color";
 import { task as s } from "../styles/component";
+import TextField from "./TextField";
+import TextInput from "./TextInput";
 // ------------------------------------------------------------
-// Task stateless component
+// Task component
 // ------------------------------------------------------------
-const Task = ({ checked, description, date, style }) => {
-    let background,
-        check,
-        outline,
-        clickable = false;
-    const { salmon, white, royalBlue } = color;
-    if (checked === true) {
-        [background, check, outline] = [salmon, white, salmon];
-    } else if (checked === false) {
-        [background, check, outline] = [white, salmon, salmon];
-    } else {
-        [background, check, outline] = [royalBlue, white, royalBlue];
-        clickable = true;
-    }
-    return (
-        <div
-            style={{
-                ...s.container,
-                ...style,
-                cursor: clickable ? "pointer" : null
-            }}
-        >
-            <div style={s.logo}>
-                <Logo
-                    width={30}
-                    height={30}
-                    background={background}
-                    check={check}
-                    outline={outline}
-                />
-            </div>
+class Task extends React.Component {
+    state = {
+        isTextField: true
+    };
+
+    alternateTextAndInput = () =>
+        this.setState({ isTextField: !this.state.isTextField });
+
+    render(
+        { checked, description, date, style } = this.props,
+        { isTextField } = this.state
+    ) {
+        let background,
+            check,
+            outline,
+            addTask = false;
+        const { salmon, white, royalBlue } = color;
+        if (checked === true) {
+            [background, check, outline] = [salmon, white, salmon];
+        } else if (checked === false) {
+            [background, check, outline] = [white, salmon, salmon];
+        } else {
+            [background, check, outline] = [royalBlue, white, royalBlue];
+            addTask = true;
+        }
+        return (
             <div
                 style={{
-                    ...s.task,
-                    ...(checked ? s.completeTask : s.incompleteTask)
+                    ...s.container,
+                    ...style,
+                    cursor: "pointer"
                 }}
             >
-                {description}
-            </div>
-            {date ? (
-                <div style={checked ? s.dateComplete : s.dateIncomplete}>
-                    {date}
+                <div style={s.logo}>
+                    <Logo
+                        width={30}
+                        height={30}
+                        background={background}
+                        check={check}
+                        outline={outline}
+                    />
                 </div>
-            ) : null}
-        </div>
-    );
-};
+                {isTextField ? (
+                    <TextField
+                        checked={checked}
+                        description={description}
+                        onClick={this.alternateTextAndInput}
+                    />
+                ) : (
+                    <TextInput
+                        onBlur={this.alternateTextAndInput}
+                        description={!addTask ? description : ""}
+                    />
+                )}
+                {date ? (
+                    <div style={checked ? s.dateComplete : s.dateIncomplete}>
+                        {moment(date).format("LL")}
+                    </div>
+                ) : null}
+                {!addTask ? (
+                    <img src="/static/delete.svg" style={s.delete} />
+                ) : null}
+            </div>
+        );
+    }
+}
 // ------------------------------------------------------------
 // prop type validation
 // ------------------------------------------------------------

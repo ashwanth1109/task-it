b95434cdf97c477dfef109ff2183fdc85ac756b9
Ashwanth A R <ashwanth1109@gmail.com>
2019-02-01 10:09:32 +0530

    Added static methods addTask and fetchTasks to Task model

--- server/models/Task.js
+++ server/models/Task.js
@@ -22,6 +22,37 @@ const taskSchema = Schema({
     }
 });
 // ------------------------------------------------------------
+// task class for api methods
+// ------------------------------------------------------------
+class TaskClass {
+    // ------------------------------------------------------------
+    // static method to create a new task
+    // ------------------------------------------------------------
+    static async addTask(description) {
+        try {
+            const task = await this.create({ description });
+            console.log(task);
+            return task;
+        } catch (err) {
+            console.log(err);
+        }
+    }
+    // ------------------------------------------------------------
+    // static method to fetch all tasks for user
+    // ------------------------------------------------------------
+    static async fetchTasks(taskIds) {
+        try {
+            return await this.find({ _id: { $in: taskIds } });
+        } catch (err) {
+            console.log(err);
+        }
+    }
+}
+// ------------------------------------------------------------
+// load class properties into schema
+// ------------------------------------------------------------
+taskSchema.loadClass(TaskClass);
+// ------------------------------------------------------------
 // create task mongoose model and export it
 // ------------------------------------------------------------
 const Task = mongoose.model("Tasks", taskSchema);

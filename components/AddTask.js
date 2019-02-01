3f6686fbd7dbc68053f8ce4b553a43d7f8daa1bc
Ashwanth A R <ashwanth1109@gmail.com>
2019-02-02 04:49:30 +0530

    Refactored AddTask to not use Task component

--- components/AddTask.js
+++ components/AddTask.js
@@ -3,22 +3,61 @@
 // ------------------------------------------------------------
 import { addTask as s } from "../styles/component";
 import withRedux from "../lib/redux/withRedux";
-import Task from "./Task";
 import { addTaskToUser } from "../lib/api/task";
+import color from "../styles/color";
+import TextField from "./TextField";
+import TextInput from "./TextInput";
+import Logo from "./Logo";
+import moment from "moment";
 // ------------------------------------------------------------
 // AddTask component
 // ------------------------------------------------------------
 class AddTask extends React.Component {
+    state = {
+        isTextField: true,
+        textValue: ""
+    };
+
+    switchToInput = () => {
+        this.setState({ isTextField: false });
+    };
+
+    switchToText = text => {
+        this.setState({ isTextField: true, textValue: text });
+    };
+
     addTask = async () => {
         const { user, addToDo, updateState } = this.props;
         user.tasks.push(await addTaskToUser(user, addToDo));
         updateState("USER", user);
     };
     render() {
+        const { isTextField } = this.state;
         return (
             <div style={s.container}>
-                <Task description="Add a task" />
-                <div style={s.section} />
+                <div style={s.taskHeader}>
+                    <div style={s.logo}>
+                        <Logo
+                            width={30}
+                            height={30}
+                            background={color.royalBlue}
+                            check={color.white}
+                            outline={color.royalBlue}
+                        />
+                    </div>
+                    {isTextField ? (
+                        <TextField
+                            checked={false}
+                            description={this.state.textValue || "Add a task"}
+                            onClick={this.switchToInput}
+                        />
+                    ) : (
+                        <TextInput
+                            onBlur={text => this.switchToText(text)}
+                            description={this.state.textValue}
+                        />
+                    )}
+                </div>
                 <div style={s.add} onClick={this.addTask} className="btn2">
                     <img
                         src="/static/add.svg"

428941051db592e8a661124f8c1290ca70a6057c
Ashwanth A R <ashwanth1109@gmail.com>
2019-02-02 19:48:05 +0530

    Comment: LabelInput

--- components/LabelInput.js
+++ components/LabelInput.js
@@ -1,30 +1,62 @@
 // ------------------------------------------------------------
 // import dependencies
 // ------------------------------------------------------------
-import { labelInput as s } from "../styles/component";
-import PropTypes from "prop-types";
-import withRedux from "../lib/redux/withRedux";
+import { labelInput as s } from "../styles/component"; // component styles
+import withRedux from "../lib/redux/withRedux"; // Redux HOC
+import PropTypes from "prop-types"; // prop type validation
 // ------------------------------------------------------------
 // LabelInput component
 // ------------------------------------------------------------
 class LabelInput extends React.Component {
-    componentDidMount(
-        { loginRefs, registerRefs, updateState, title, loginForm } = this.props
-    ) {
+    // ------------------------------------------------------------
+    // componentDidMount => store refs in redux state
+    // ------------------------------------------------------------
+    componentDidMount() {
+        // ------------------------------------------------------------
+        // extract props
+        // ------------------------------------------------------------
+        const {
+            loginRefs,
+            registerRefs,
+            updateState,
+            title,
+            loginForm
+        } = this.props;
+        // ------------------------------------------------------------
+        // check if label input is for login or register section
+        // ------------------------------------------------------------
         if (loginForm) {
+            // ------------------------------------------------------------
+            // store login input field refs in redux state
+            // ------------------------------------------------------------
             loginRefs[title] = this.refs[title];
             updateState("LOGIN_REFS", loginRefs);
         } else {
+            // ------------------------------------------------------------
+            // store register input field refs in redux state
+            // ------------------------------------------------------------
             registerRefs[title] = this.refs[title];
             updateState("REGISTER_REFS", registerRefs);
         }
     }
-    render({ icon, title, type, onFocus, onBlur } = this.props) {
+    // ------------------------------------------------------------
+    // render method
+    // ------------------------------------------------------------
+    render() {
+        // ------------------------------------------------------------
+        // extract props
+        // ------------------------------------------------------------
+        const { icon, title, type, onFocus, onBlur } = this.props;
+        // ------------------------------------------------------------
+        // return component
+        // ------------------------------------------------------------
         return (
             <div style={s.container}>
+                {/* INPUT ICON */}
                 <div style={s.label}>
                     <img src={icon} style={s.icon} alt="user" />
                 </div>
+                {/* INPUT FIELD */}
                 <input
                     type={type}
                     placeholder={title}

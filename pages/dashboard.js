e3f459769c09ffe9d791df8df6b6dac5dd14a453
Ashwanth A R <ashwanth1109@gmail.com>
2019-02-03 00:30:52 +0530

    Refactor + Comments: dashboard page

--- pages/dashboard.js
+++ pages/dashboard.js
@@ -1,64 +1,51 @@
 // ------------------------------------------------------------
 // import dependencies
 // ------------------------------------------------------------
-import withRedux from "../lib/redux/withRedux";
-import Router from "next/router";
-import { dashboard as s } from "../styles/component";
+import { dashboard as s } from "../styles/component"; // component styles
+import withRedux from "../lib/redux/withRedux"; // REDUX HOC
+import Router from "next/router"; // next router
+import moment from "moment"; // moment js
+// ------------------------------------------------------------
+// import components
+// ------------------------------------------------------------
 import Card from "../components/Card";
-import * as moment from "moment";
 import AddTask from "../components/AddTask";
 import TaskList from "../components/TaskList";
 // ------------------------------------------------------------
-// dashboard page stateless component
+// dashboard page class component
 // ------------------------------------------------------------
 class Dashboard extends React.Component {
-    componentDidMount({ user, updateState } = this.props) {
+    // ------------------------------------------------------------
+    // componentDidMount => if user doesnt exist, reroute to login page
+    // ------------------------------------------------------------
+    componentDidMount({ user } = this.props) {
         if (user === null) {
             Router.push("/");
-            // updateState("USER", {
-            //     name: "Test User",
-            //     "tasks": [
-            //         {
-            //             "description": "Complete wireframes for Task It",
-            //             "date": "January 31, 2019",
-            //             "checked": true
-            //         },
-            //         {
-            //             "description": "Complete designs for Task It",
-            //             "date": "January 31, 2019",
-            //             "checked": true
-            //         },
-            //         {
-            //             "description": "Complete frontend for Task It",
-            //             "date": "January 31, 2019",
-            //             "checked": false
-            //         },
-            //         {
-            //             "description":
-            //                 "This is a super long task and it should get cut off by the overflow property. This is a super long task and it should get cut off by the overflow property.",
-            //             "date": "January 31, 2019",
-            //             "checked": false
-            //         }
-            //     ],
-            //     username: "test"
-            // });
         }
     }
+    // ------------------------------------------------------------
+    // render method => only render if user exists
+    // ------------------------------------------------------------
     render({ user } = this.props) {
         if (user === null) {
             return null;
         } else {
             return (
                 <div style={s.container}>
+                    {/* CARD FOR ADD TASK */}
                     <Card style={s.cardContainer}>
+                        {/* HEADER MESSAGE */}
                         <div style={s.header}>
                             <div style={s.title}>Welcome, {user.name}</div>
                             <div style={s.date}>{moment().format("LL")}</div>
                         </div>
+                        {/* ADD TASK COMPONENT */}
                         <AddTask />
                     </Card>
                     <div style={s.spacer} />
+                    {/* CARD FOR TASK LIST */}
                     <Card style={s.cardContainer}>
+                        {/* TASK LIST COMPONENT */}
                         <TaskList />
                     </Card>
                     <div style={s.spacer} />
@@ -68,6 +55,6 @@ class Dashboard extends React.Component {
     }
 }
 // ------------------------------------------------------------
-// export dashboard
+// export dashboard withRedux => mapState only
 // ------------------------------------------------------------
-export default withRedux(Dashboard, true, true);
+export default withRedux(Dashboard, true, false);

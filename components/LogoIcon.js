f3fd064849d4864c483b2b9d06ee3388200fd0f2
Ashwanth A R <ashwanth1109@gmail.com>
2019-02-03 00:34:03 +0530

    Created LogoIcon component

---
+++ components/LogoIcon.js
@@ -0,0 +1,29 @@
+// ------------------------------------------------------------
+// import dependencies
+// ------------------------------------------------------------
+import { logoIcon as s } from "../styles/component"; // component styles
+import PropTypes from "prop-types"; // prop type validation
+// ------------------------------------------------------------
+// import components
+// ------------------------------------------------------------
+import Logo from "./Logo";
+// ------------------------------------------------------------
+// Logo Icon stateless component
+// ------------------------------------------------------------
+const LogoIcon = ({ background, check, outline, onClick, animationTiming }) => (
+    <div style={s.container}>
+        <Logo
+            width={30}
+            height={30}
+            background={background}
+            check={check}
+            outline={outline}
+            onClick={onClick ? onClick : null}
+            animationTiming={animationTiming ? animationTiming : 0}
+        />
+    </div>
+);
+// ------------------------------------------------------------
+// export LogoIcon
+// ------------------------------------------------------------
+export default LogoIcon;

5431f5b9a74b2953672480933a8a81340ad8e7f7
Ashwanth A R <ashwanth1109@gmail.com>
2019-02-03 00:29:02 +0530

    Remove unused colors from palette

--- styles/color.js
+++ styles/color.js
@@ -3,17 +3,11 @@
 // ------------------------------------------------------------
 const color = {
     white: "#fff",
-    cream: "#fafafa",
     lightGray: "#BDBDBD",
-    orange: "#F66031",
-    blue: "#2b88d9",
-    lightBlue: "#4094dd",
     royalBlue: "#595ce2",
     lightRoyal: "#686AE4",
     salmon: "#f27475",
     coral: "#F38081",
-    gainsboro: "#d6d6e4",
-    grey: "#9E9E9E",
     black: "#222"
 };
 // ------------------------------------------------------------

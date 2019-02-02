1181322f9a393cd2e39358edb35dea1b5f8488cd
Ashwanth A R <ashwanth1109@gmail.com>
2019-02-02 19:47:25 +0530

    Comment + Prop Validation: Card

--- components/Card.js
+++ components/Card.js
@@ -1,13 +1,24 @@
 // ------------------------------------------------------------
 // import dependencies
 // ------------------------------------------------------------
-import { card } from "../styles/component";
+import { card } from "../styles/component"; // component styles
+import PropTypes from "prop-types"; // prop type validation
 // ------------------------------------------------------------
 // Card component
 // ------------------------------------------------------------
 const Card = ({ children, style }) => (
     <div style={{ ...card, ...style }}>{children}</div>
 );
+// ------------------------------------------------------------
+// prop type validation
+// ------------------------------------------------------------
+Card.propTypes = {
+    children: PropTypes.oneOfType([
+        PropTypes.arrayOf(PropTypes.node),
+        PropTypes.node
+    ]).isRequired,
+    style: PropTypes.object
+};
 // ------------------------------------------------------------
 // export Card
 // ------------------------------------------------------------
